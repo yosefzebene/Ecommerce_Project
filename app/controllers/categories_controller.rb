af802b49bac72d594c1f909bb7e3f6c74ff6377f
yosef-ZzZ <yosef.zebene1@gmail.com>
2022-11-27 18:32:00 -0600

    Category and product show pages

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,7 +1,10 @@
 class CategoriesController < ApplicationController
   def index
+    @categories = Category.all
   end
 
   def show
+    @category = Category.find(params[:id])
+    @products_in_category = @category.products.where(isactive: true)
   end
 end
--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,6 +4,6 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @products = Product.where(isactive: true).find(params[:id])
+    @product = Product.where(isactive: true).find(params[:id])
   end
 end
