fab68ebd9b18cfe658283487067026ea8503626c
yosef-ZzZ <yosef.zebene1@gmail.com>
2022-12-02 18:47:24 -0600

    Got tests to pass

--- test/controllers/products_controller_test.rb
+++ test/controllers/products_controller_test.rb
@@ -2,12 +2,12 @@ require "test_helper"
 
 class ProductsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get products_index_url
+    get products_url
     assert_response :success
   end
 
   test "should get show" do
-    get products_show_url
+    get products_url
     assert_response :success
   end
 end
