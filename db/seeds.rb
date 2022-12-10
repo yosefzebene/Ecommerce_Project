93b5bb573ad0d3ca65f008298acf0eaf2d6deee9
yosef-ZzZ <yosef.zebene1@gmail.com>
2022-12-09 20:20:20 -0600

    Update seeding file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,68 +1,70 @@
 require "csv"
 
-# Seed country, region, and city
-# Region.destroy_all
-# Country.destroy_all
+# Seed country, regions and taxes
+TaxRate.destroy_all
+Region.destroy_all
+Country.destroy_all
 
-# country = Country.create(name: Faker::Address.country)
+csv_file = Rails.root.join('db/province_taxes.csv')
+csv_data = File.read(csv_file)
 
-# 5.times do
-#  country.regions.create(name: Faker::Address.state, code: Faker::Address.state_abbr)
-# end
+provinces = CSV.parse(csv_data, headers: true)
 
-# Rails.logger.debug "1 Country, 5 Regions, and 10 cities created."
+provinces.each do |province|
+    country = Country.find_or_create_by!(name: province['country'])
+
+    region = country.regions.create(name: province['province'], code: province['code'])
+
+    TaxRate.create(region_id: region.id, GST: province['gst'], PST: province['pst'], HST: province['hst'])
+end
+
+Rails.logger.debug "Provinces and tax rates imported from CSV"
 
 # Seed products and categories
-# ProductsCategory.destroy_all
-# Product.destroy_all
-# Category.destroy_all
+ProductsCategory.destroy_all
+Product.destroy_all
+Category.destroy_all
 
-# 5.times do
-#     category = Category.find_or_create_by!(name: Faker::Commerce.unique.department(max: 1))
+csv_file = Rails.root.join('db/products.csv')
+csv_data = File.read(csv_file)
 
-#     2.times do
-#         product = Product.new(name: Faker::Commerce.unique.product_name,
-#                          price: Faker::Commerce.price(range: 1000..10_000),
-#                          description: Faker::Marketing.buzzwords, isactive: true)
-#         # downloaded_image = URI.open("https://source.unsplash.com/600x600/?#{product.name}")
-#         # product.image.attach(io: downloaded_image, filename: "m-#{product.name}.jpg")
-#         product.save
+products = CSV.parse(csv_data, headers: true)
 
-#         ProductsCategory.create(product_id: product.id, category_id: category.id)
-#     end
-# end
+products.each do |csvproduct|
+    #Create the product
+    product = Product.create!(
+        name: csvproduct['name'],
+        price: csvproduct['price'],
+        description: csvproduct['description'],
+        isactive: csvproduct['isactive']
+    )
 
-# Rails.logger.debug "5 categories and 10 products created."
+    image = File.open("#{Rails.root}/db/product_images/#{csvproduct['image']}")
+    sleep(0.2)
 
-# csv_file = Rails.root.join('db/products.csv')
-# csv_data = File.read(csv_file)
+    product.image.attach(
+        io: image,
+        filename: "m-#{product.name}.jpg"
+    )
 
-# products = CSV.parse(csv_data, headers: true)
+    # Create the categories
+    category_names = csvproduct['category']
+    category_names = category_names.split(",")
+    category_names.each do |c|
+        category = Category.find_or_create_by!(name: c)
 
-# products.each do |csvproduct|
-#     #Create the product
-#     product = Product.create!(
-#         name: csvproduct['name'],
-#         price: csvproduct['price'],
-#         description: csvproduct['description'],
-#         isactive: csvproduct['isactive']
-#     )
+        ProductsCategory.create!(product_id: product.id, category_id: category.id)
+    end
+end
 
-#     product.image.attach(
-#         io: File.open("#{Rails.root}/db/product_images/#{csvproduct['image']}"),
-#         filename: "m-#{product.name}.jpg"
-#     )
+Rails.logger.debug "Products CSV imported"
 
-#     # Create the categories
-#     category_names = csvproduct['category']
-#     category_names = category_names.split(",")
-#     category_names.each do |c|
-#         category = Category.find_or_create_by!(name: c)
+# Seed Status
+Status.destroy_all
 
-#         ProductsCategory.create!(product_id: product.id, category_id: category.id)
-#     end
-# end
-# puts "Products CSV imported"
+Status.create(name: "New", description: "Newly created orders without payment confirmation.")
+Status.create(name: "Paid", description: "Payment have been confirmed by stripe.")
+Status.create(name: "Shipped", description: "Order has been shipped.")
 
 # AdminUser.create!(
 #   email: 'admin@example.com',
